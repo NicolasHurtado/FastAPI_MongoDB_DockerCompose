da8b95e3c349b4727199405546d812b9038b43a8
NicolasHurtado <nicolashurtado0712@gmail.com>
2024-04-16 14:26:58 -0500

    Add  mongodb

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import httpx
 import pandas as pd
+from pymongo import MongoClient
+import traceback
+
+# Configurar la conexión a MongoDB
+client = MongoClient("mongodb://mongodb:27017/")
+db = client["mydatabase"]
+matches_collection = db["matches"]
 
 app = FastAPI()
 API_KEY = "d75e1a9df5b24ef7b38212d36842ecde"  # Reemplaza "TU_CLAVE_DE_API" con tu propia clave
@@ -11,10 +18,13 @@ async def root():
 
 
 
-@app.get("/matches")
-async def get_matches():
+@app.get("/filter_matches_date_goals/")
+async def get_matches(request: Request):
+    date_init = request.query_params.get("date_init")
+    date_end = request.query_params.get('date_end')
+    goals = request.query_params.get('goals',0)
     async with httpx.AsyncClient() as client:
-        url = "https://api.football-data.org/v4/matches?competitions=PL&status=FINISHED&dateFrom=2024-04-13&dateTo=2024-04-15"
+        url = f"https://api.football-data.org/v4/matches?competitions=PL&status=FINISHED&dateFrom={date_init}&dateTo={date_end}"
         headers = {"X-Auth-Token": API_KEY}
         response = await client.get(url, headers=headers)
         data = response.json()
@@ -22,15 +32,30 @@ async def get_matches():
     
     matches_df = pd.DataFrame(data["matches"])
     matches_df["total_goals"] = matches_df.apply(lambda row: row["score"]["fullTime"]["home"] + row["score"]["fullTime"]["away"], axis=1)
-    filtered_matches_df = matches_df[matches_df["total_goals"] >= 2]
+    filtered_matches_df = matches_df[matches_df["total_goals"] >= int(goals)]
 
     filtered_matches = filtered_matches_df[["homeTeam", "awayTeam", "matchday", "score", "utcDate"]]
-    filtered_matches["Marcador_final"] = filtered_matches.apply(lambda row: f"{row['score']['fullTime']['home']} - {row['score']['fullTime']['away']}", axis=1)
-    filtered_matches["Cuando"] = pd.to_datetime(filtered_matches["utcDate"]).dt.strftime('%Y-%m-%d %H:%M:%S')
-    filtered_matches["Local"] = filtered_matches["homeTeam"].apply(lambda x: x["name"])
-    filtered_matches["Visitante"] = filtered_matches["awayTeam"].apply(lambda x: x["name"])
-    filtered_matches = filtered_matches[["Local", "Visitante", "matchday", "Marcador_final", "Cuando"]]
-    filtered_matches = filtered_matches.rename(columns={"matchday": "Fecha"})
-
+    filtered_matches["Score"] = filtered_matches.apply(lambda row: f"{row['score']['fullTime']['home']} - {row['score']['fullTime']['away']}", axis=1)
+    filtered_matches["When"] = pd.to_datetime(filtered_matches["utcDate"]).dt.strftime('%Y-%m-%d %H:%M:%S')
+    filtered_matches["HomeTeam"] = filtered_matches.apply(lambda row: {"id": row["homeTeam"]["id"], "name": row["homeTeam"]["name"]}, axis=1)
+    filtered_matches["AwayTeam"] = filtered_matches.apply(lambda row: {"id": row["awayTeam"]["id"], "name": row["awayTeam"]["name"]}, axis=1)
+    filtered_matches = filtered_matches[["HomeTeam", "AwayTeam", "matchday", "Score", "When"]]
+    filtered_matches = filtered_matches.rename(columns={"matchday": "Matchday"})
+    # Convertir los datos filtrados a una lista de diccionarios
+    matches_data = filtered_matches.to_dict(orient="records")
+    
+    # Insertar los registros en la colección de MongoDB
+    matches_collection.insert_many(matches_data)
     return filtered_matches.to_dict(orient="records")
+
+
+@app.get("/list")
+async def get_list(request: Request):
+    # Consultar todos los registros de la colección de MongoDB
+    matches_data = list(matches_collection.find())
+    # Convertir ObjectId a cadenas en los diccionarios
+    for match in matches_data:
+        match["_id"] = str(match.get("_id"))
+
+    return matches_data
 
